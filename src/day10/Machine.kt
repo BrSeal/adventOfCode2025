239058a4339bcd91e097c0873c4973e5c86529a2
Alex Melnikov <melnikov@progredis.ru>
2025-12-14 20:44:06 +0300

    Day 10 part 1

--- src/day10/Machine.kt
+++ src/day10/Machine.kt
@@ -1,47 +1,28 @@
 package day10
 
 class Machine(
-    val targetState: BooleanArray,
-    val buttons:List<BooleanArray>,
-    val joltage: List<Int>,
-    val currentState: BooleanArray = BooleanArray(targetState.size),
+    val targetState: Int,
+    val buttons:List<Int>,
+    val targetJoltage: IntArray
 ) {
-    companion object {
-        const val ON = '#'
+    companion object { const val ON = '#'
        fun fromStringArray(arr:List<String>): Machine{
            val targetState = arr[0].replace("[]\\[]".toRegex(), "")
-               .map { it == ON }.toBooleanArray()
+               .map { it == ON }.toBooleanArray().joinToString("") { if(it) "1" else "0" }.toInt(2)
            val buttons = arr.slice(1..<arr.lastIndex)
                .map {
-                   val button = BooleanArray(targetState.size)
+                   val button = BooleanArray(arr[0].length - 2)
                    it.replace("[()]".toRegex(), "").split(",")
                        .map { it.toInt() }
                        .forEach { button[it] = true }
-                   button
+                   button.joinToString("") { if(it) "1" else "0" }.toInt(2)
                }
-           val joltage = arr.last().replace("[{}]".toRegex(), "").split(",")
-               .map{ it.toInt()}
+           val targetJoltage = arr.last().replace("[{}]".toRegex(), "").split(",")
+               .map{ it.toInt()}.toIntArray()
 
-           return Machine(targetState, buttons, joltage)
+           return Machine(targetState, buttons, targetJoltage)
        }
 
 
-    }
-
-    fun push(i: Int){
-        val button = buttons[i]
-        for(i in button.indices){
-            if(button[i]){
-                currentState[i] = !currentState[i]
-            }
-        }
-    }
-
-    override fun toString(): String {
-        return getStateStr()
-    }
-
-    fun getStateStr(): String{
-        return currentState.joinToString("", "[", "]") { if(it) "#" else "." }
     }
 }
--- src/day10/Main.kt
+++ src/day10/Main.kt
@@ -2,14 +2,45 @@ package day10
 
 import java.io.File
 import java.util.ArrayDeque
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 
 val input = File("src/day10/input.tst").readLines()
     .map { it.split(" ") }
     .map { Machine.fromStringArray(it) }
 
 fun main() {
+    input.sumOf { calcButtonPresses(it) }
+        .also { println("Part 1: $it") }
+}
 
+fun calcButtonPresses(m: Machine): Int {
+    var minPresses = m.buttons.size
+
+    val queue = ArrayDeque<Triple<Int, Int, List<Int>>>()
+    queue.add(Triple(0, 0, m.buttons))
+
+    while (queue.isNotEmpty()) {
+        val iteration = queue.pop()
+        val currentState = iteration.first
+        val iterationNum = iteration.second
+        val availableButtons = iteration.third
+
+        if (availableButtons.isEmpty()) {
+            continue
+        }
+
+        if (iterationNum >= minPresses) {
+            continue
+        }
+
+        if (currentState == m.targetState) {
+            minPresses = iterationNum
+            continue
+        }
+        m.buttons.forEach { button ->
+            val buttons = availableButtons.filter { it != button }
+            queue.push(Triple(currentState xor button, iterationNum + 1, buttons))
+        }
+    }
+
+    return minPresses
 }
