038753c0cb2431a92733db1580f4b202bb46266c
Alexander Melnikov <Alexander.L.Melnikov@kaspersky.com>
2026-01-13 22:52:00 +0300

    Day 11 part 2

--- src/day11/Main.kt
+++ src/day11/Main.kt
@@ -3,14 +3,16 @@ package day11
 import java.io.File
 import java.util.ArrayDeque
 
-val START = "you"
-val FINISH = "out"
+val YOU = "you"
+val OUT = "out"
+val SVR = "svr"
+val DAC = "dac"
+val FFT = "fft"
 
 val input = File("src/day11/input").readLines()
     .associate {
         it.split(": ").let { strArr -> strArr[0] to strArr[1].split(" ").toMutableList() }
     }
-    .filterReachable(START)
     .toMutableMap()
 
 fun main() {
@@ -18,22 +20,30 @@ fun main() {
     println("Part 2: ${part2()}")
 }
 
-fun part1(): Int {
-    return countPaths()
+fun part1(): Long {
+    return countPaths(YOU, OUT)
 }
 
 fun part2(): Long {
-    return 0
+    val srvToDac = countPaths(SVR, OUT)
+    val dacToFft = countPaths(DAC, FFT)
+    val fftToOut = countPaths(FFT, OUT)
+
+    val srvToFft = countPaths(SVR, FFT)
+    val fftToDac = countPaths(FFT, DAC)
+    val dacToOut = countPaths(DAC, OUT)
+    return srvToDac * dacToFft * fftToOut + srvToFft * fftToDac * dacToOut
 }
 
-fun countPaths(): Int {
-    val parentNodes = input.invert()
-    val pathCounts = mutableMapOf<String, Int>()
-    pathCounts[START] = 1
+fun countPaths(from: String, to:String): Long {
+    val readableInput = input.filterReachable(from)
+    val parentNodes = readableInput.filterReachable(from).invert()
+    val pathCounts = mutableMapOf<String, Long>()
+    pathCounts[from] = 1
     val visited = mutableSetOf<String>()
 
     val queue = ArrayDeque<String>()
-    queue.add(START)
+    queue.add(from)
     while (queue.isNotEmpty()) {
         val node = queue.pop()
 
@@ -47,26 +57,26 @@ fun countPaths(): Int {
             continue
         }
 
-        input[node]?.forEach {
+        readableInput[node]?.forEach {
             pathCounts[it] = if(pathCounts[it] == null) pathCounts[node]!! else pathCounts[it]!! + pathCounts[node]!!
             queue.add(it)
         }
 
         visited.add(node)
     }
-    return pathCounts[FINISH]!!
+    return pathCounts[to]?:0
 }
 
 fun Map<String, List<String>>.filterReachable(from: String): Map<String, List<String>> {
     val reachableNodes = mutableSetOf<String>()
 
     val queue = ArrayDeque<String>()
-    queue.add(START)
+    queue.add(from)
 
     while (queue.isNotEmpty()) {
         val node = queue.pop()
         if (reachableNodes.add(node)) {
-            get(node)?.forEach { queue.add(it) }
+            this[node]?.forEach { queue.add(it) }
         }
     }
 
